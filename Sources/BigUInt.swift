5528e51580ccb8943fec770216d8f3c4940549af
Karoly Lorentey <karoly@lorentey.hu>
2015-12-30 21:03:16 +0100

    Implement BigUInt division using Knuth's algorithm.

--- Sources/BigUInt.swift
+++ Sources/BigUInt.swift
@@ -596,3 +596,172 @@ public func >> (b: BigUInt, amount: Int) -> BigUInt {
 }
 
 
+//MARK: Quotient and Remainder
+
+extension BigUInt {
+    public var width: Int {
+        guard count > 0 else { return 0 }
+        return (count - 1) * Int(Digit.width) + Int(self[count - 1].rank)
+    }
+    public var numberOfLeadingZeroes: Int {
+        guard count > 0 else { return 0 }
+        return Int(Digit.width - self[count - 1].rank)
+    }
+
+    /// Divides this integer by `y`, leaving the quotient in its place and returning the remainder.
+    /// - Complexity: O(count)
+    @warn_unused_result
+    public mutating func divideInPlaceByDigit(y: Digit) -> Digit {
+        precondition(y > 0)
+        if y == 1 { return 0 }
+        lift()
+
+        var remainder: Digit = 0
+        for i in (0..<count).reverse() {
+            let u = self[i]
+            let q = Digit.fullDivide(remainder, u, y)
+            self[i] = q.div
+            remainder = q.mod
+        }
+        return remainder
+    }
+
+    @warn_unused_result
+    public static func divmodByDigit(x: BigUInt, _ y: Digit) -> (div: BigUInt, mod: Digit) {
+        var div = x
+        let mod = div.divideInPlaceByDigit(y)
+        return (div, mod)
+    }
+
+    /// Divide `x` by `y` and return the resulting quotient and remainder.
+    /// - Requires: y > 0
+    /// - Returns: (div, mod) where div = floor(x/y), mod = x - div * y
+    /// - Complexity: O(x.count * y.count)
+    @warn_unused_result
+    public static func divmod(x: BigUInt, _ y: BigUInt) -> (div: BigUInt, mod: BigUInt) {
+        // This is a Swift adaptation of "divmnu" from Hacker's Delight, which is in
+        // turn a C adaptation of Knuth's Algorithm D (TAOCP vol 2, 4.3.1).
+
+        precondition(y.count > 0)
+
+        // First, let's take care of the easy cases.
+
+        if x.count < y.count {
+            return (0, x)
+        }
+        if y.count == 1 {
+            // The single-digit case reduces to a simpler loop.
+            let (div, mod) = divmodByDigit(x, y[0])
+            return (div, BigUInt(mod))
+        }
+        
+        // This function simply performs the long division algorithm we learned in school.
+        // It works by successively calculating the remainder of the top y.count digits of x
+        // under y, and subtracting it from the top of x while remembering the quotient.
+        //
+        // The tricky part is that the algorithm needs to be able to divide two equal-sized
+        // big integers, but we only have a primitive for dividing two digits by a single
+        // digit. (Remember that this step is also tricky when we do it on paper!)
+        //
+        // The solution is that the long division can be approximated by a single fullDivide
+        // using just the most significant digits. We can then use multiplies and subtracts
+        // to refine the approximation until we get the correct quotient digit.
+
+        // We could do this by doing a simple 2/1 fullDivide, but Knuth goes one step further,
+        // and implements a 3/2 division. This results in an exact approximation in the 
+        // vast majority of cases, eliminating the need for some long subtractions.
+        // Here is the code for the 3/2 division:
+
+        /// Return the 3/2-sized quotient `x/y` as a single Digit.
+        /// - Requires: (x.0, x.1) <= y && y.0.high != 0
+        /// - Returns: Digit.max when the quotient doesn't fit in a single digit, or an exact value.
+        func approximateQuotient(x x: (Digit, Digit, Digit), y: (Digit, Digit)) -> Digit {
+            // Start with q = (x.0, x.1) / y.0, (or Digit.max on overflow)
+            var q: Digit
+            var r: Digit
+            if x.0 == y.0 {
+                q = Digit.max
+                let (s, o) = Digit.addWithOverflow(x.0, x.1)
+                if o { return q }
+                r = s
+            }
+            else {
+                let (d, m) = Digit.fullDivide(x.0, x.1, y.0)
+                q = d
+                r = m
+            }
+            // Now refine q by considering x.2 and y.1. 
+            // Note that since y is normalized, q - x/y is between 0 and 2.
+            var p = Digit.fullMultiply(q, y.1)
+            while p.0 > r || (p.0 == r || p.1 > x.2) {
+                q -= 1
+                let (a, ao) = Digit.addWithOverflow(r, y.0)
+                if ao { return q }
+                r = a
+                let (s, so) = Digit.subtractWithOverflow(p.1, y.1)
+                if so { p.0 -= 1 }
+                p.1 = s
+            }
+            return q
+        }
+
+        // The function above requires that the divisor's most significant digit is larger than
+        // Digit.max / 2. This ensures that the approximation has tiny error bounds,
+        // which is what makes this entire approach viable.
+        // To satisfy this requirement, we can simply normalize the division by multiplying
+        // both the divisor and the dividend by the same (small) factor.
+        let z = y.numberOfLeadingZeroes
+        let divisor = y << z
+        var remainder = x << z // We'll calculate the remainder in the normalized dividend.
+        var quotient = BigUInt()
+        assert(divisor.count == y.count && divisor.last!.high > 0)
+
+        // Note that normalization also ensures that the top digit of `remainder`
+        // will not be greater than the top digit of `divisor`--which is the other
+        // requirement of the 3/2 approximation.
+        assert(remainder.count >= x.count && remainder.last! <= divisor.last!)
+
+        // We're ready to start the long division!
+        let dc = divisor.count
+        let d1 = divisor[dc - 1]
+        let d0 = divisor[dc - 2]
+        for j in (0 ... remainder.count - dc).reverse() {
+
+            // Approximate dividing the top dc digits of remainder using the topmost 3/2 digits.
+            let r2 = remainder[dc + j]
+            let r1 = remainder[dc + j - 1]
+            let r0 = remainder[dc + j - 2]
+            let q = approximateQuotient(x: (r2, r1, r0), y: (d1, d0))
+
+            // Multiply q with the whole divisor and subtract the result from remainder.
+            // Normalization ensures the 3/2 quotient will either be exact for the full division, or
+            // it may overshoot by at most 1, in which case the product will be greater
+            // than remainder.
+            let product = divisor.multiplyByDigit(q)
+            if product <= remainder[j ... dc + j] {
+                remainder.subtractInPlace(product, shift: j)
+                quotient[j] = q
+            }
+            else {
+                // This case is extremely rare -- it has a probability of 1/2^(Digit.width - 1).
+                remainder.subtractInPlace(product - divisor, shift: j)
+                quotient[j] = q - 1
+            }
+        }
+        // The remainder's normalization needs to be undone, but otherwise we're done.
+        return (quotient, remainder >> z)
+    }
+}
+
+@warn_unused_result
+public func /(x: BigUInt, y: BigUInt) -> BigUInt {
+    return BigUInt.divmod(x, y).div
+}
+
+@warn_unused_result
+public func %(x: BigUInt, y: BigUInt) -> BigUInt {
+    return BigUInt.divmod(x, y).mod
+}
+
+public func /=(inout x: BigUInt, y: BigUInt) { x = x / y }
+public func %=(inout x: BigUInt, y: BigUInt) { x = x % y }
