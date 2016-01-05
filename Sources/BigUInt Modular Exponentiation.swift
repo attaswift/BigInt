4ee096552a56f64d654db721e11fdab636fc6d5a
Karoly Lorentey <karoly@lorentey.hu>
2016-01-05 19:56:52 +0100

    Add vanilla exponentiation

--- Sources/BigUInt Modular Exponentiation.swift
+++ Sources/BigUInt Modular Exponentiation.swift
@@ -9,11 +9,42 @@
 import Foundation
 
 extension BigUInt {
-    //MARK: Modular Exponentiation
-    
-    /// Returns the remainder of `base` raised to the power `exponent` under `modulus`.
+    //MARK: Exponentiation
+
+    /// Returns this integer raised to the power `exponent`.
     ///
-    /// - Complexity: O(exponent.count * modulus.count^log2(3)) or so
+    /// This function calculates the result by [successively squaring the base while halving the exponent][expsqr].
+    ///
+    /// [expsqr]: https://en.wikipedia.org/wiki/Exponentiation_by_squaring
+    ///
+    /// - Note: This function can be unreasonably expensive for large exponents, which is why `exponent` is
+    ///         a simple integer value. If you want to calculate big exponents, you'll probably need to use
+    ///         the modulo arithmetic variant.
+    /// - SeeAlso: `BigUInt.power(_:, modulus:)`
+    /// - Complexity: O((exponent * self.count)^log2(3)) or somesuch. The result may require a large amount of memory, too.
+    @warn_unused_result
+    public func power(exponent: Int) -> BigUInt {
+        if exponent == 0 { return 1 }
+        var result = BigUInt(1)
+        var b = self
+        var e = exponent
+        while e > 0 {
+            if e & 1 == 1 {
+                result = (result * b)
+            }
+            e >>= 1
+            b = (b * b)
+        }
+        return result
+    }
+
+    /// Returns the remainder of this integer raised to the power `exponent` in modulo arithmetic under `modulus`.
+    ///
+    /// Uses the [right-to-left binary method][rtlb].
+    ///
+    /// [rtlb]: https://en.wikipedia.org/wiki/Modular_exponentiation#Right-to-left_binary_method
+    ///
+    /// - Complexity: O(exponent.count * modulus.count^log2(3)) or somesuch
     @warn_unused_result
     public func power(exponent: BigUInt, modulus: BigUInt) -> BigUInt {
         if modulus == 1 { return 0 }
