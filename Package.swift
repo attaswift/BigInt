dcfe0fdad1d2d7c407172876a95313b9ddc7b832
Karoly Lorentey <karoly@lorentey.hu>
2016-01-12 07:31:04 +0100

    Add Package.swift file for Swift Package Manager.

---
+++ Package.swift
@@ -0,0 +1,11 @@
+//
+//  Package.swift
+//  BigInt
+//
+//  Created by Károly Lőrentey on 2016-01-12.
+//  Copyright © 2016 Károly Lőrentey. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(name: "BigInt", dependencies: [])
