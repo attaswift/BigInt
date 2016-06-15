7d569306fc8adca92a3bca5c17dc4ab26b7e3acf
Karoly Lorentey <karoly@lorentey.hu>
2016-06-15 12:02:28 +0200

    Upgrade playground for Swift 3.0 in Xcode 8.0 beta 1.

--- Demo.playground/Pages/Generating Large Primes.xcplaygroundpage/Contents.swift
+++ Demo.playground/Pages/Generating Large Primes.xcplaygroundpage/Contents.swift
@@ -1,17 +1,13 @@
 //: [Previous](@previous)
-
 import Foundation
 import BigInt
-
 //: # Generating Large Prime Numbers
 //:
 //: `BigUInt` has an `isPrime()` method that does a [Miller-Rabin Primality Test][mrpt]. Let's use
 //: this to create a function that finds the next prime number after any integer:
 //:
 //: [mrpt]: https://en.wikipedia.org/wiki/Miller%2dRabin_primality_test
-
-
-func findNextPrimeAfter(integer: BigUInt) -> BigUInt {
+func findNextPrime(after integer: BigUInt) -> BigUInt {
     var candidate = integer
     repeat {
         candidate.increment()
@@ -19,12 +15,11 @@ func findNextPrimeAfter(integer: BigUInt) -> BigUInt {
     return candidate
 }
 
-findNextPrimeAfter(100)
-findNextPrimeAfter(1000)
-findNextPrimeAfter(10000)
-findNextPrimeAfter(100000000000)
-findNextPrimeAfter(1 << 64)
-findNextPrimeAfter(1 << 128)
-findNextPrimeAfter(1 << 256)
-
+findNextPrime(after: 100)
+findNextPrime(after: 1000)
+findNextPrime(after: 10000)
+findNextPrime(after: 100000000000)
+findNextPrime(after: BigUInt(1) << 64)
+findNextPrime(after: BigUInt(1) << 128)
+findNextPrime(after: BigUInt(1) << 256)
 //: [Next](@next)
--- Demo.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ Demo.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -1,7 +1,5 @@
-import Foundation
-
-//: # Introduction 
-
+//: # Introduction
+//:
 //: The `BigInt` module provides a `BigInt` type that implements an [https://en.wikipedia.org/wiki/Arbitrary-precision_arithmetic](integer type of arbitrary width).
 //: These work much like `Int`s, but they don't have a preset maximum value---so they will never overflow.
 //: The only practical limit to their capacity is the amount of memory & address space that your computer has, and the
@@ -9,19 +7,15 @@ import Foundation
 //: big integer operations can take a long time to execute.
 //: (`BigInt` represents integers in base 2^64, storing digits in an `Array<UInt64>`, so the theoretical
 //: maximum value it can store is (2^64)^`Int.max` - 1.)
-
+import Foundation
 import BigInt
-
 //: `BigInt` has several interesting initializers, but for now, the simplest way to create big integers is to use integer
 //: or string literals. The latter is useful when you want to create a number that's larger than `UIntMax.max`:
-
 let a: BigInt = 123
 let b: BigInt = 12345678
 let c: BigInt = 1234567890123456
 let d: BigInt = "12345678901234567890123456789012345678901234567890123456789012345678"
-
 //: To work with `BigInt`s, you use the same arithmetic operators as you do with everyday `Int` values:
-
 a + b
 b - a
 -b
@@ -34,6 +28,4 @@ d % c
 d / (c * c)
 d / (c * c * c)
 d / (c * c * c * c)
-
-
 //: [Next](@next)
