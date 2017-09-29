5acedc9d6953c9f50e2d45dbc4ed833802556944
Andrew Hannon <andrew@valt.io>
2017-09-29 11:27:32 -0400

    kind/storage private => fileprivate

--- sources/BigUInt.swift
+++ sources/BigUInt.swift
@@ -27,8 +27,8 @@ public struct BigUInt: UnsignedInteger {
         case array
     }
 
-    internal private(set) var kind: Kind // Internal for testing only
-    internal private(set) var storage: [Word] // Internal for testing only; stored separately to prevent COW copies
+    internal fileprivate (set) var kind: Kind // Internal for testing only
+    internal fileprivate (set) var storage: [Word] // Internal for testing only; stored separately to prevent COW copies
 
     /// Initializes a new BigUInt with value 0.
     public init() {
