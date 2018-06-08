671df3142b0e6f286671e2fdd753412411f57f04
Koray Koska <koray@koska.at>
2018-06-08 14:24:46 +0200

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -15,9 +15,7 @@ let package = Package(
         .library(name: "BigInt", targets: ["BigInt"])
     ],
     dependencies: [
-      // @attaswift: remove this pragma after you have fixed SipHash!
-      .package(url: "https://github.com/dankogai/SipHash", .branch("master"))
-        //.package(url: "https://github.com/attaswift/SipHash", from: "1.2.0")
+        .package(url: "https://github.com/attaswift/SipHash", from: "1.2.1")
     ],
     targets: [
         .target(name: "BigInt", dependencies: ["SipHash"], path: "sources"),
