fdbd681ffc4b99176a400aef9ebe0d97ecab6a74
Tim Gymnich <TG908@users.noreply.github.com>
2019-04-02 14:55:53 +0200

    Revert "Swift 5 migration"
    This reverts commit 6b4b715c2dd741cfdfc18c087e973b0bf656c447.

--- sources/Data Conversion.swift
+++ sources/Data Conversion.swift
@@ -61,7 +61,7 @@ extension BigUInt {
             index -= 1
         }
         var word: Word = 0
-        data.withUnsafeBytes { p in
+        data.enumerateBytes { p, byteIndex, stop in
             for byte in p {
                 word <<= 8
                 word += Word(byte)
@@ -87,7 +87,7 @@ extension BigUInt {
         guard byteCount > 0 else { return Data() }
 
         var data = Data(count: byteCount)
-        data.withUnsafeMutableBytes { p in
+        data.withUnsafeMutableBytes { (p: UnsafeMutablePointer<UInt8>) -> Void in
             var i = byteCount - 1
             for var word in self.words {
                 for _ in 0 ..< Word.bitWidth / 8 {
