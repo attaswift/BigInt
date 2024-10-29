07d522be48a284ecc2ee679350cdab095da6b5c7
Michael Redig <mredig@gmail.com>
2024-10-29 03:23:21 -0500

    reduce Foundation dependency

--- Sources/Data Conversion.swift
+++ Sources/Data Conversion.swift
@@ -6,7 +6,9 @@
 //  Copyright © 2016-2017 Károly Lőrentey.
 //
 
+#if canImport(Foundation)
 import Foundation
+#endif
 
 extension BigUInt {
     //MARK: NSData Conversion
@@ -43,7 +45,33 @@ extension BigUInt {
         assert(c == 0 && word == 0 && index == -1)
     }
 
+    /// Return a `UnsafeRawBufferPointer` buffer that contains the base-256 representation of this integer, in network (big-endian) byte order.
+    public func serializeToBuffer() -> UnsafeRawBufferPointer {
+        // This assumes Digit is binary.
+        precondition(Word.bitWidth % 8 == 0)
 
+        let byteCount = (self.bitWidth + 7) / 8
+
+        let buffer = UnsafeMutableBufferPointer<UInt8>.allocate(capacity: byteCount)
+
+        guard byteCount > 0 else { return UnsafeRawBufferPointer(start: buffer.baseAddress, count: 0) }
+
+        var i = byteCount - 1
+        for var word in self.words {
+            for _ in 0 ..< Word.bitWidth / 8 {
+                buffer[i] = UInt8(word & 0xFF)
+                word >>= 8
+                if i == 0 {
+                    assert(word == 0)
+                    break
+                }
+                i -= 1
+            }
+        }
+        return UnsafeRawBufferPointer(start: buffer.baseAddress, count: byteCount)
+    }
+
+    #if canImport(Foundation)
     /// Initializes an integer from the bits stored inside a piece of `Data`.
     /// The data is assumed to be in network (big-endian) byte order.
     public init(_ data: Data) {
@@ -82,31 +110,15 @@ extension BigUInt {
 
     /// Return a `Data` value that contains the base-256 representation of this integer, in network (big-endian) byte order.
     public func serialize() -> Data {
-        // This assumes Digit is binary.
-        precondition(Word.bitWidth % 8 == 0)
+        let buffer = serializeToBuffer()
+        defer { buffer.deallocate() }
+        guard
+            let pointer = buffer.baseAddress.map(UnsafeMutableRawPointer.init(mutating:))
+        else { return Data() }
 
-        let byteCount = (self.bitWidth + 7) / 8
-
-        guard byteCount > 0 else { return Data() }
-
-        var data = Data(count: byteCount)
-        data.withUnsafeMutableBytes { buffPtr in
-            let p = buffPtr.bindMemory(to: UInt8.self)
-            var i = byteCount - 1
-            for var word in self.words {
-                for _ in 0 ..< Word.bitWidth / 8 {
-                    p[i] = UInt8(word & 0xFF)
-                    word >>= 8
-                    if i == 0 {
-                        assert(word == 0)
-                        break
-                    }
-                    i -= 1
-                }
-            }
-        }
-        return data
+        return Data(bytes: pointer, count: buffer.count)
     }
+    #endif
 }
 
 extension BigInt {
@@ -133,7 +145,29 @@ extension BigInt {
 
         self.magnitude = BigUInt(UnsafeRawBufferPointer(rebasing: buffer.dropFirst(1)))
     }
-    
+
+    /// Return a `Data` value that contains the base-256 representation of this integer, in network (big-endian) byte order and a prepended byte to indicate the sign (0 for positive, 1 for negative)
+    public func serializeToBuffer() -> UnsafeRawBufferPointer {
+        // Create a data object for the magnitude portion of the BigInt
+        let magnitudeBuffer = self.magnitude.serializeToBuffer()
+
+        // Similar to BigUInt, a value of 0 should return an empty buffer
+        guard magnitudeBuffer.count > 0 else { return magnitudeBuffer }
+
+        // Create a new buffer for the signed BigInt value
+        let newBuffer = UnsafeMutableRawBufferPointer.allocate(byteCount: magnitudeBuffer.count + 1, alignment: 8)
+        let magnitudeSection = UnsafeMutableRawBufferPointer(rebasing: newBuffer[1...])
+        magnitudeSection.copyBytes(from: magnitudeBuffer)
+        magnitudeBuffer.deallocate()
+
+        // The first byte should be 0 for a positive value, or 1 for a negative value
+        // i.e., the sign bit is the LSB
+        newBuffer[0] = self.sign == .plus ? 0 : 1
+
+        return UnsafeRawBufferPointer(start: newBuffer.baseAddress, count: newBuffer.count)
+    }
+
+    #if canImport(Foundation)
     /// Initializes an integer from the bits stored inside a piece of `Data`.
     /// The data is assumed to be in network (big-endian) byte order with a first
     /// byte to represent the sign (0 for positive, 1 for negative)
@@ -160,20 +194,13 @@ extension BigInt {
     
     /// Return a `Data` value that contains the base-256 representation of this integer, in network (big-endian) byte order and a prepended byte to indicate the sign (0 for positive, 1 for negative)
     public func serialize() -> Data {
-        // Create a data object for the magnitude portion of the BigInt
-        let magnitudeData = self.magnitude.serialize()
-        
-        // Similar to BigUInt, a value of 0 should return an initialized, empty Data struct
-        guard magnitudeData.count > 0 else { return magnitudeData }
-        
-        // Create a new Data struct for the signed BigInt value
-        var data = Data(capacity: magnitudeData.count + 1)
-        
-        // The first byte should be 0 for a positive value, or 1 for a negative value
-        // i.e., the sign bit is the LSB
-        data.append(self.sign == .plus ? 0 : 1)
-        
-        data.append(magnitudeData)
-        return data
+        let buffer = serializeToBuffer()
+        defer { buffer.deallocate() }
+        guard
+            let pointer = buffer.baseAddress.map(UnsafeMutableRawPointer.init(mutating:))
+        else { return Data() }
+
+        return Data(bytes: pointer, count: buffer.count)
     }
+    #endif
 }
